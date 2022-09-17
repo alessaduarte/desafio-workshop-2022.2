c70a4619e4f527fbc388f5afbb0deb3fb454bf67
alessaduarte <alessadanubia@gmail.com>
2022-09-16 23:59:58 -0300

    Projeto Final

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,8 +4,10 @@ function About() {
   return (
     <div className='container'>
         <h1>Zoo</h1>
+      <div className="about-container">
         <img src="https://images.unsplash.com/photo-1557431011-7fc07385c834?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" alt="" />
         <p className="information">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae libero eum sint deserunt repellendus eaque totam iste enim nam ducimus illum, repudiandae doloremque tenetur optio voluptatibus facere amet cupiditate autem!</p>
+      </div>
     </div>
   );
 }
